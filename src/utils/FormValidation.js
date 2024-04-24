9826efcc1b4ca87d3f8d77f43abddbe931ed8850
devjunaeid <junaeid.md48@gmail.com>
2024-04-24 12:33:44 +0600

    Added: Phone and Email Validation.

--- src/utils/FormValidation.js
+++ src/utils/FormValidation.js
@@ -1,17 +1,31 @@
+export const CheckInput = (formInput) => {
+  const error = {};
 
-export const CheckInput = (currentStep, formInput) => {
-    const error = {}
+  // Regex pattern for checking name.
+  var nameRegex = /^[a-zA-Z ]{2,30}$/;
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const phoneRegex = /^([+]\d{2}[ ])?\d{11}$/
 
-    // Regex pattern for checking name.
-    var nameRegex = /^[a-zA-Z ]{2,30}$/;
+  //   Name Validaton
+  if (!formInput.name || formInput.name.trim().length === 0) {
+    error.name = "Name is required";
+  } else if (!nameRegex.test(formInput.name)) {
+    error.name = "Enter a valid name(only letters, min 2char)";
+  }
 
-    if (currentStep === 1) {
-       if (!formInput.name || formInput.name.trim().length === 0) {
-          error.name = "Name is required"
-       }else if (!nameRegex.test(formInput.name)){
-         error.name = "Enter a valid name(only letters, min 2char)"
-       }
-    }
+  //   Phone Validation
+  if (!formInput.email || formInput.phone.trim().length === 0) {
+    error.phone = "Phone number is required!";
+  } else if (!phoneRegex.test(formInput.phone)) {
+    error.phone = "Enter a valid phone number!";
+  }
 
-    return error
-}
+  //   Email Validation
+  if (!formInput.email || formInput.email.trim().length === 0) {
+    error.email = "Email is required";
+  } else if (!emailRegex.test(formInput.email)) {
+    error.email = "Enter a valid email!";
+  }
+
+  return error;
+};
