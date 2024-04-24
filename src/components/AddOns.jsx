f9749b873e303fe7325ddbda4cffa41c8ae70d24
devjunaeid <junaeid.md48@gmail.com>
2024-04-24 12:30:01 +0600

    Added: yearly & monthly pricing based on plan

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -2,41 +2,51 @@ import { useContext, useEffect, useState } from "react";
 import { FormContext } from "../Context";
 
 // Addons
-const addons = [
-  {
+const addons = {
+  "Online Service": {
     id: 1,
-    name: "Online Service",
     des: "Access to multiplayer games.",
-    price: 1,
+    price_monthly: 1,
+    price_yearly: 10,
   },
-  {
+  "Extra Storage": {
     id: 2,
-    name: "Extra Storage",
     des: "Extra 1TB of Cloud Storage.",
-    price: 2,
+    price_monthly: 2,
+    price_yearly: 20,
   },
-  {
+  "Customizable Profile": {
     id: 3,
-    name: "Customizable Profile",
     des: "Custom theme on your profile.",
-    price: 2,
+    price_monthly: 2,
+    price_yearly: 20,
   },
-];
+};
 
 function AddOns() {
   const { formData, setFormData } = useContext(FormContext);
-  const [addedService, setAddedService] = useState({});
-  
+  const [addedService, setAddedService] = useState({
+    type: formData.plan?.type,
+  });
+
   // Handle Input Change
-  const handleChange = (item) => {
-    if (addedService[item.name] != undefined) {
+  const handleChange = (key, item) => {
+    if (addedService[key] != undefined) {
       var temp = addedService;
-      delete temp[item.name];
-      setFormData({...formData, addons: temp});
+      delete temp[key];
+      setFormData({ ...formData, addons: temp });
       setAddedService(temp);
     } else {
-      var temp = { ...addedService, [item.name]: { price: item.price } };
-      setFormData({...formData, addons: temp});
+      var temp = {
+        ...addedService,
+        [key]: {
+          price:
+            formData.plan?.type === "yearly"
+              ? item.price_yearly
+              : item.price_monthly,
+        },
+      };
+      setFormData({ ...formData, addons: temp });
       setAddedService(temp);
     }
   };
@@ -44,37 +54,62 @@ function AddOns() {
   // Getting addons if it was already choosen
   useEffect(() => {
     if (Object.keys(formData.addons).length != 0) {
-      setAddedService(formData.addons);
+      // Check If plan type changes in step 2.
+      if (formData.plan?.type === formData.addons.type) {
+        //If plan type remain same
+        setAddedService(formData.addons); // Setting addon data.
+      } else {
+        var temp = { type: formData.plan?.type };
+        Object.entries(formData.addons).forEach(([key, value]) => {
+          if (key != "type") {
+            temp[key] = {
+              price:
+                formData.plan?.type === "yearly"
+                  ? addons[key].price_yearly
+                  : addons[key].price_monthly,
+            };
+          }
+        });
+        setAddedService(temp);
+      }
     }
   }, []);
 
   return (
     <div className="w-full flex flex-col gap-4 mt-4">
-      {addons.map((item) => (
+      {Object.entries(addons).map(([key, item]) => (
         <div
           className={`flex justify-center items-center gap-4 px-3 py-3 rounded-md md:gap-8 md:px-8 md:py-4 hover:border hover:border-blue-500 ${
-            addedService[item.name] !=undefined ? "border-2 border-blue-700" : "border-2 border-gray-300/25"
+            addedService[item.name] != undefined
+              ? "border-2 border-blue-700"
+              : "border-2 border-gray-300/25"
           }`}
           key={item.id}
         >
           <input
             type="checkbox"
             className="w-5 h-5 accent-blue-600"
-            onChange={() => handleChange(item)}
-            checked={addedService[item.name] !=undefined}
+            onChange={() => handleChange(key, item)}
+            checked={addedService[key] != undefined}
           />
           <div className="w-full flex justify-between items-center">
             <div className="flex flex-col justify-start align-baseline">
-              <p className="text-lg md:text-xl font-semibold">{item.name}</p>
-              <p className="text-base md:text-md font-normal text-gray-400">{item.des}</p>
+              <p className="text-lg md:text-xl font-semibold">{key}</p>
+              <p className="text-base md:text-md font-normal text-gray-400">
+                {item.des}
+              </p>
             </div>
-            <p className="text-sm md:text-base text-blue-500">+${item.price}/mo</p>
+            <p className="text-sm md:text-base text-blue-500">
+              +$
+              {formData.plan?.type === "yearly"
+                ? `${item.price_yearly}/yr`
+                : `${item.price_monthly}/mo`}
+            </p>
           </div>
         </div>
       ))}
     </div>
-  )
-  ;
+  );
 }
 
 export default AddOns;
